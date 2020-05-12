4b83fb75db40edf31ea58c68b6eb17a10660a0d1
Muhamar <amararrasyid@gmail.com>
2020-05-12 18:28:10 +0700

    Tugas 24 Javascript
    Mengumpulkan Tugas Niomic

---
+++ index.js
@@ -0,0 +1,12 @@
+function panggilSort() {
+    var numbers = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+
+    console.log("Sebelumnya : " + numbers);
+    asc = "Ascending : " + numbers.sort();
+    console.log(asc);
+
+    desc = "Descending : " + numbers.reverse();
+    console.log(desc);
+}
+
+panggilSort()
